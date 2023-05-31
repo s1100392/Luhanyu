43dd1750c0bec76d6c1428a796bf7bdea9ee1a93
v1 <s1100392@gm.pu.edu.tw>
2023-05-31 10:43:57 +0800

    v.2

---
+++ app/src/main/java/com/example/luhanyu/ComposeTheme.kt
@@ -0,0 +1,5 @@
+package com.example.luhanyu
+
+class ComposeTheme(any: Any) {
+
+}
---
+++ app/src/main/java/com/example/luhanyu/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.luhanyu
+
+import android.graphics.fonts.Font
+import android.graphics.fonts.FontFamily
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.camera.core.Preview
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import android.graphics.Color as Color1
+import java.lang.reflect.Modifier as Modifier1
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
